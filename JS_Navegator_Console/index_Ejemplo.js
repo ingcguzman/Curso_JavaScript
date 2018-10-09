41ac555e11fc8dbacc2967b96001791dbbef5f14
Carlos Guzmán <ingcguzman@hotmail.com>
2018-10-08 21:13:11 -0500

    Actualizacion con JavaScript Navegadores y consola

---
+++ JS_Navegator_Console/index_Ejemplo.js
@@ -0,0 +1,7 @@
+function escribirEnConsola(){
+  console.log('Hola desde la consola JavaScript')
+}
+/*setinterval(escribirEnConsola,1000)*/
+escribirEnConsola()
+var vble1 = 'Texto de prueba'
+var vble2 = 10
