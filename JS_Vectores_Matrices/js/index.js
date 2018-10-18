6c0dd6a174f239ca0f7690ea2918f48ee7689b35
Carlos Guzmán <ingcguzman@hotmail.com>
2018-10-17 21:51:48 -0500

    Actualización con JavaScript Vectores y Matrices

---
+++ JS_Vectores_Matrices/js/index.js
@@ -0,0 +1,7 @@
+var fila1 = ["Carlos", 12, 40];
+var fila2 = ["Camilo", 45, 73];
+var fila3 = ["Luis", 27, 85];
+var fila4 = ["Pablo", 78, 80];
+var matriz = [fila1, fila2, fila3, fila4];
+console.log("Primera fila de la matriz " + matriz[0]);
+console.log("Posicion 0,1 de la matriz " + matriz[0][1]);
---
+++ JS_Vectores_Matrices/js/index_1.js
@@ -0,0 +1,11 @@
+/*-- Concatenación de vectores */
+var vector1 = [23, 45, 67];
+var vector2 = [43, 60, 70];
+console.log("vector concatenado " + vector1.concat(vector2));
+/* -- Funcion Splice --*/
+var letras = ["a", "b", "c", "d", "e", "f"];
+console.log("splice 1,3 " + letras.splice(1,3));
+/*-- Funcion Slice */
+console.log("slice 1,3 " + letras.slice(1,3));
+/*-- Vector letras despues de aplicar las funciones */
+console.log("vector letras " + letras);
