996a423967eb7c537116c9d27495cd662577ff69
Carlos Guzmán <ingcguzman@hotmail.com>
2018-10-17 21:50:29 -0500

    Actualizació con JavaScript Números

---
+++ JS_Numeros/js/index.js
@@ -0,0 +1,7 @@
+var promedio = 0, max = 0, min = 0;
+promedio = (2.95 + 3.128 + 4.0 + parseFloat("5"));
+max = Math.max(2.95, 3.128, 4.0, parseFloat("5"));
+min = Math.min(2.95, 3.128, 4.0, parseFloat("5"));
+console.log("promedio: "+promedio);
+console.log("Max: "+max);
+console.log("Min: "+min);
