89bda7f668b94a29ab7ead6435734209419c4bf0
Carlos Guzmán <ingcguzman@hotmail.com>
2018-10-17 21:45:05 -0500

    Actualización JavaScript con Ejercicio de codificación de introducción a la rpogramación

---
+++ JS_Codifica_Introd_Programacion/js/index.js
@@ -0,0 +1,31 @@
+document.getElementById('boton-receta1').addEventListener('click', function(){
+  var receta1="Pie de Limón";
+  var ingrediente1="Claras de huevo", ingrediente2="Azucar blanca", ingrediente3="Leche condensada",
+      ingrediente4="Gelatina sin sabor", ingrediente5="Limones";
+  var cantidad1=5, cantidad2=150, cantidad3=200,ccantidad4=1, cantidad5=4;
+  var unidad1="unidades", unidad2="gramos", unidad3="gramos", unidad4="sobre", unidad5="unidades";
+  console.log('Receta para preparar un '+receta1);
+  console.log("Ingredientes               Cantidad");
+  console.log("Claras de huevo            5 unidades");
+  console.log("Azucar blanca              150 gramos");
+  console.log("Leche condensada           200 gramos");
+  console.log("Gelatina sin sabor         1 sobre");
+  console.log("Limones                    4 unidades");
+  console.log("Preparación:");
+  console.log("Primero debes adicionar las "+ingrediente1+" en un bol y batir con los "+cantidad2+" "+unidad2+ " de "+
+               ingrediente2+";");
+  console.log("luego debes adicionar los "+cantidad3+" "+unidad3+" de "+ingrediente3);
+
+
+
+
+})
+
+document.getElementById('boton-receta2').addEventListener('click', function(){
+
+
+
+
+
+
+})
